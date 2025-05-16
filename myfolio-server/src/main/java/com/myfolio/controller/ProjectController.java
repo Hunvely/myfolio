f7a2819f933d5b1afcf3e03772d9be2241f71fdb
Hunvely <jihoon9611@gmail.com>
2025-05-16 15:08:32 +0900

    refactor: Change deleteProject response type from Project to Void

--- myfolio-server/src/main/java/com/myfolio/controller/ProjectController.java
+++ myfolio-server/src/main/java/com/myfolio/controller/ProjectController.java
@@ -43,7 +43,7 @@ public class ProjectController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Project> deleteProject(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteProject(@PathVariable Long id) {
         projectService.deleteUser(id);
         return ResponseEntity.noContent().build();
     }
