469060bf82d6372c2729d21ce5c206801d701c2b
Hunvely <jihoon9611@gmail.com>
2025-10-28 15:09:30 +0900

    feat(router): add basic router configuration style(app): add App.vue styles and update common style.css

--- myfolio-client/src/router/index.js
+++ myfolio-client/src/router/index.js
@@ -1,10 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import SignUpPage from '../views/SignUpPage.vue'
+import Login from '../views/Login.vue'
+import DashboardPage from '../views/DashBoardPage.vue'
+
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
-  { path: '/signup', name: 'SignUp', component: SignUpPage }
+  { path: '/home', name: 'HomePage', component: Home },
+  { path: '/signup', name: 'SignUp', component: SignUpPage },
+  { path: '/login', name: 'Login', component: Login },
+  { path: '/dashboard', name: 'Dashboard', component: DashboardPage }
 ]
 
 const router = createRouter({
