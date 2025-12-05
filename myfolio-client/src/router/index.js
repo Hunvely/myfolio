15f813fd25b91019125e0a0ceee0a182fd4f22e0
Hunvely <jihoon9611@gmail.com>
2025-12-05 16:22:46 +0900

    feat(router): add new router path

--- myfolio-client/src/router/index.js
+++ myfolio-client/src/router/index.js
@@ -3,6 +3,9 @@ import Home from '../views/Home.vue'
 import SignUpPage from '../views/SignUpPage.vue'
 import Login from '../views/Login.vue'
 import DashboardPage from '../views/DashBoardPage.vue'
+import PortfolioList from '../views/PortfolioList.vue'
+import CreatePortfolio from '../views/CreatePortfolio.vue'
+import MyPage from '../views/MyPage.vue'
 
 
 const routes = [
@@ -10,7 +13,10 @@ const routes = [
   { path: '/home', name: 'HomePage', component: Home },
   { path: '/signup', name: 'SignUp', component: SignUpPage },
   { path: '/login', name: 'Login', component: Login },
-  { path: '/dashboard', name: 'Dashboard', component: DashboardPage }
+  { path: '/dashboard', name: 'Dashboard', component: DashboardPage },
+  { path: '/dashboard/portfolios', name: 'PortfolioList', component: PortfolioList },
+  { path: '/portfolio/create', name: 'CreatePortfolio', component: CreatePortfolio },
+  { path: '/dashboard/profile', name: 'MyPage', component: MyPage }
 ]
 
 const router = createRouter({
